a6ea63699643d69f0d1911948a513caaa42fd322
Ye Myo Aung <yemyo994@gmail.com>
2022-10-29 21:03:22 +0630

    day6

--- bin/useful.dart
+++ bin/useful.dart
@@ -1,5 +1,20 @@
 void main() {
-  String name = "abcdefgc";
+  String name = "a";
+
+  // print(name.indexOf('c'));
+
+  // print(name.indexOf('c',4));
+
+// print(name.length);
+print("name");
+print("x");
+print(name.padRight(4,' '));
+// print(name.trim());
+// name.trimLeft();
+// name.trimRight();
+
+
+
   // print(name.startsWith('b',1));
   // print(name.contains('cd',3));
   // name.endsWith('c');
@@ -7,7 +22,9 @@ void main() {
 //  final String newName = name.replaceAll('ab', 'ba');
 //  print(newName);
 
-  print(name.replaceFirst("c", "h", 8));
+  // print(name.replaceFirst("c", "h", 8));
+
+  // name.
 
   // final String lowerName = name.toLowerCase();
   // print(lowerName);
@@ -22,3 +39,7 @@ void main() {
 
   ///in
 }
+
+
+
+    
