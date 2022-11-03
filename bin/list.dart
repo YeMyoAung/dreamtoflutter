acf17d0fcd32677093205281fcd129bb93021dae
Ye Myo Aung <yemyo994@gmail.com>
2022-11-03 20:12:48 +0630

    Thu 3 Nov

---
+++ bin/list.dart
@@ -0,0 +1,94 @@
+void main() {
+  ///var ကဘာနဲ့တူလဲဆိုရင် late သဘောတရားတူတယ်
+  ///var ကဘယ်လိုအလုပ်လုပ်လဲဆိုရင် နီးစပ်ရာDataTypeကိုပြောင်းပြီးအလုပ်လုပ်ပေးတယ်
+
+  // var hello = {};
+  // print(hello.runtimeType);
+
+// var fajslfas = "hello";
+// dynamic fdjkafjds = "hello";
+// fajslfas= "jdklfajlsda";
+// fdjkafjds = 1;
+
+// var fajsdlfsa;
+//  fajsdlfsa = 1;
+//  fajsdlfsa = 'fjdas';
+
+  // dynamic students = {
+  //   'map':1
+  // };
+
+// print(students.subString(10));
+
+  // students.length;
+  // print(students.reversed.toList());
+  ///mgmg == MgMg
+  // students.hashCode;
+  // print('mgmg'.hashCode);
+  // print('mgmG'.hashCode);
+  // print(fasjfsal().runtimeType);
+
+// list.first;
+// list.last;
+// list.isEmpty;
+// list.isNotEmpty;
+// {fasfas,f,dfsa,fasd,}
+// list.iterator;
+// print(list.single);
+// list.add({});
+// list.addAll([1,2,3,4,54,5,6,]);
+
+// hello(hi);
+  // print();
+  // print(list.contains('mg mg'));
+  // list.contains('Mg Mg');
+
+  // list.contains('1');
+  // list.contains(2);
+
+
+
+  // print(list.asMap());
+//  List castList = list.cast();
+//    print(list);
+//    print(castList);
+//    print("--------");
+//  list.add("hello");
+//     print(list);
+//    print(castList);
+// print( toDoList.any((incoming) {
+//     return incoming == 'Mg Mg' || incoming == 1 || incoming == 2;
+//   }));
+// print(toDoList.every((incoming){
+//  return incoming == 'Mg Mg' || incoming == 1 || incoming == 2;
+// }));
+// list.clear();
+// print(list);
+  List toDoList = ["work","learn","freelance","rest"];
+  List complete = ["work","learn","freelance"];
+//   toDoList.contains(complete);
+// print(
+//    toDoList.every((fasfsa){
+//   return complete.contains(fasfsa);
+//  })
+// );
+print(toDoList.any((jfdlajs){
+  return complete.contains(jfdlajs);
+}));
+}
+
+
+
+
+
+// var h = (dfjaslfdjkas) {
+//   return dfjaslfdjkas == 'mg mg' || dfjaslfdjkas == 1;
+// };
+
+// bool Function(dynamic) test
+// int hi(){}
+
+
+// void hello(Function fsafasd){
+
+// }
--- bin/useful.dart
+++ bin/useful.dart
@@ -1,23 +1,47 @@
 void main() {
-  String name = "a";
+  String name = "dart is a language";
 
   // print(name.indexOf('c'));
 
   // print(name.indexOf('c',4));
 
 // print(name.length);
-print("name");
-print("x");
-print(name.padRight(4,' '));
+// print("name");
+// print("x");
+// print(name.padRight(4,' '));
 // print(name.trim());
 // name.trimLeft();
 // name.trimRight();
+///index number => list
+// print(name.substring(10,13));
+// print(name.split(
+//   ' '
+// ));
+// print(name.replaceRange(0, 4, 'Flutter'));
+// final double afds = 2.01;
+// print(afds.ceilToDouble());
+// print(afds.floorToDouble());
+// print(afds.toInt());
+// print(1.toDouble());
+// print(afds.compareTo(3));///-1 0 1
+// afds.toStringAsFixed(fractionDigits);
+
+// print(int.parse(name));
+// print(double.parse(name));
+// print(int.tryParse(name));
+double.infinity;
+// double.maxFinite;
+// double.minPositive;
+// double.nan;
+// double.negativeInfinity;
 
 
 
+// print(double.tryParse(name));
   // print(name.startsWith('b',1));
   // print(name.contains('cd',3));
   // name.endsWith('c');
+
 
 //  final String newName = name.replaceAll('ab', 'ba');
 //  print(newName);
